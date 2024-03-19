1f3d25be615a58a08ee89c96cb487840953ca795
CallMeMacx <Zero102321@gmail.com>
2024-03-19 20:12:38 +0700

    4 direction Player Animation

---
+++ Asset/Components/PlayerMoveComponent.gd
@@ -0,0 +1,41 @@
+class_name MoveComponent
+extends CharacterBody2D
+
+@export var speed: float
+@export var animation_tree: AnimationTree 
+@export var sprite_Character2D: CharacterBody2D
+
+var direction = Vector2.ZERO
+
+func _ready() -> void:
+	animation_tree.active = true
+	
+func _process(delta: float) -> void:
+	update_aimation_parameter()
+
+func _physics_process(delta: float) -> void:
+	
+	# Move in 4 directions
+	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
+	
+	if direction:
+		velocity = direction * speed
+	else:
+		velocity = Vector2.ZERO
+		
+	move_and_slide()
+
+
+func update_aimation_parameter():
+	# Find path at "AnimationTree Inspector -> Parameters -> Conditions
+	if(velocity == Vector2.ZERO):
+		animation_tree["parameters/conditions/idle"] = true
+		animation_tree["parameters/conditions/is_moving"] = false
+	else:
+		animation_tree["parameters/conditions/idle"] = false
+		animation_tree["parameters/conditions/is_moving"] = true
+	
+	if(velocity != Vector2.ZERO):
+		animation_tree["parameters/Idel/blend_position"] = direction
+		animation_tree["parameters/walk/blend_position"] = direction
+	
