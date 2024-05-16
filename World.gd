d1abcd142b08db7c020b72172f00b80f9c048ac0
CallMeMacx <Zero102321@gmail.com>
2024-05-16 14:18:13 +0700

    Add UI
    - UI Slot

--- World.gd
+++ World.gd
@@ -5,9 +5,11 @@ const MINITES_PER_HOUR = 60
 const INGAME_TO_REAL_MINUTE_DURATION = (2 * PI) / MINUTES_PER_DAY
 
 @onready var daylight: CanvasModulate = %Daylight
-@onready var water: CanvasModulate = %Water
+@onready var waterlight: CanvasModulate = %Water
+
 @onready var day_time_label: Control = $CanvasLayer/DayTimeLabel
 @export var gradient: GradientTexture1D
+
 @export var INGAME_SPEED = 1.0
 @export var INITIAL_HOUR = 12:
 	set(h):
@@ -21,6 +23,9 @@ var past_minute:float = -1.0
 
 
 func _ready() -> void:
+	daylight.visible = true
+	waterlight.visible = true
+	
 	time = INGAME_TO_REAL_MINUTE_DURATION * INITIAL_HOUR * MINITES_PER_HOUR
 	time_tick.connect(day_time_label.set_daytime)
 
@@ -29,7 +34,7 @@ func _process(delta: float) -> void:
 	time += delta * INGAME_TO_REAL_MINUTE_DURATION * INGAME_SPEED
 	var value = (sin(time - PI / 2 ) + 1.0) / 2.0
 	daylight.color = gradient.gradient.sample(value)
-	water.color = gradient.gradient.sample(value)
+	waterlight.color = gradient.gradient.sample(value)
 	
 	_reacalculate_time()
 	
